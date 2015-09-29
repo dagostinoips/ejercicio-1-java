beaad1e7ea389b3c1d817a5fc67c9a4800de4801
Mariano D'Agostino <dagostinoips@gmail.com>
2015-09-28 21:03:46 -0300

    arreglado el test

---
+++ test/CronometroTest.java
@@ -0,0 +1,16 @@
+package Cronometro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import uk.co.placona.Cronometro.Cronometro;
+
+public class CronometroTest {
+	@Test
+	public void testHellowWorld(){
+		Cronometro cronometro = new Cronometro();
+    cronometro.reiniciar();
+		assertEquals("Probar el cronometro en cero", "0 minutos 0 segundos", cronometro.mostrarValor());
+	}
+}
